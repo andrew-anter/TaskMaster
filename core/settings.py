a997231f90e5f6d237f468f534902c4b3fc8952a
Andrew Anter <andrew.anter@gmail.com>
2025-05-31 12:09:37 +0300

    added django-htmx package for htmx support

--- core/settings.py
+++ core/settings.py
@@ -27,6 +27,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "django_htmx",
     "todo",
 ]
 
@@ -38,6 +39,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "django_htmx.middleware.HtmxMiddleware",
 ]
 
 ROOT_URLCONF = "core.urls"
