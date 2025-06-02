b2e028a24216047b8020ce6653017e685760441b
Andrew Anter <andrew.anter@gmail.com>
2025-06-02 07:23:52 +0300

    purged unused imports

--- accounts/views.py
+++ accounts/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_not_required
 from django.shortcuts import redirect
 from django.urls import reverse
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import login
 from .forms import CustomLoginForm
 
 
