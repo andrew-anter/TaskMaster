624f62dd3dcbf337973639a30271ce461998a468
Andrew Anter <andrew.anter@gmail.com>
2025-06-02 07:03:20 +0300

    hooked up the accounts app

--- core/settings.py
+++ core/settings.py
@@ -91,6 +91,7 @@ AUTH_PASSWORD_VALIDATORS = [
         "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
+LOGIN_URL = "accounts/login/"
 
 LANGUAGE_CODE = "en-us"
 TIME_ZONE = "UTC"
--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.shortcuts import redirect
+# from django.shortcuts import redirect
 
 from django.conf import settings  # For static files in DEBUG
 from django.conf.urls.static import static
@@ -8,7 +8,8 @@ from django.conf.urls.static import static
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("tasks/", include("todo.urls")),
-    path("", lambda _: redirect("task_list", permanent=False)),
+    path("accounts/", include("accounts.urls")),
+    # path("", lambda _: redirect("task_list", permanent=False)),
 ]
 
 # Serve static and media files during development
