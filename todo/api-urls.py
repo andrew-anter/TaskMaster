7c38a8f5498e242bbdb7d46d05ef03ea6eea084a
Andrew Anter <andrew.anter@gmail.com>
2025-06-21 21:00:42 +0300

    Created DetailTaskApi to get specific task details

--- todo/api-urls.py
+++ todo/api-urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .api import ListTasksAPI
+from .api import DetailTaskAPI, ListTasksAPI
 
 urlpatterns = [
     path("", ListTasksAPI.as_view(), name="list-tasks-api"),
+    path("<int:pk>", DetailTaskAPI.as_view(), name="get-task-api"),
 ]
--- todo/api.py
+++ todo/api.py
@@ -1,46 +1,48 @@
 from rest_framework import serializers
 from rest_framework.authentication import BasicAuthentication, SessionAuthentication
 from rest_framework.permissions import IsAuthenticated
-from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.views import APIView
+
 from .models import Task
+from .selectors import get_all_tasks_for_user, get_task_for_user
 
-from .selectors import get_all_tasks_for_user
 
-
+# TODO: add support for token authentication
 class BaseAPIView(APIView):
     authentication_classes = [SessionAuthentication, BasicAuthentication]
     permission_classes = [IsAuthenticated]
 
 
-# TODO: add support for token authentication
-class ListTasksAPI(BaseAPIView):
-    class TaskSerializer(serializers.ModelSerializer):
-        class Meta:
-            model = Task
-            fields = [
-                "title",
-                "description",
-                "status",
-                "priority",
-                "due_datetime",
-                "scheduled_date",
-                "created_at",
-                "modified_at",
-            ]
+class TaskSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Task
+        fields = [
+            "title",
+            "description",
+            "status",
+            "priority",
+            "due_datetime",
+            "scheduled_date",
+            "created_at",
+            "modified_at",
+        ]
 
+
+class ListTasksAPI(BaseAPIView):
     def get(self, request):
         user = request.user
         tasks = get_all_tasks_for_user(user=user)
-
-        data = self.TaskSerializer(tasks, many=True).data
-
+        data = TaskSerializer(tasks, many=True).data
         return Response(data)
 
 
-# TODO: GetTaskAPI
-class GetTaskAPI(BaseAPIView):
-    pass
+class DetailTaskAPI(BaseAPIView):
+    def get(self, request, pk: int):
+        user = request.user
+        task = get_task_for_user(user=user, task_id=pk)
+        data = TaskSerializer(task).data
+        return Response(data)
 
 
 # TODO: UpdateTakskAPI
