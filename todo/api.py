29533f7fb75fa9a85cfa651428a682ae02b5b5a3
Andrew Anter <andrew.anter@gmail.com>
2025-06-21 19:24:10 +0300

    created an api for listing tasks

--- todo/api.py
+++ todo/api.py
@@ -1,1 +1,36 @@
+from rest_framework import serializers
+from rest_framework.authentication import BasicAuthentication, SessionAuthentication
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import Task
 
+from .selectors import get_all_tasks_for_user
+
+
+# TODO: add support for token authentication
+class ListTasks(APIView):
+    authentication_classes = [SessionAuthentication, BasicAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    class TaskSerializer(serializers.ModelSerializer):
+        class Meta:
+            model = Task
+            fields = [
+                "title",
+                "description",
+                "status",
+                "priority",
+                "due_datetime",
+                "scheduled_date",
+                "created_at",
+                "modified_at",
+            ]
+
+    def get(self, request):
+        user = request.user
+        tasks = get_all_tasks_for_user(user=user)
+
+        data = self.TaskSerializer(tasks, many=True).data
+
+        return Response(data)
