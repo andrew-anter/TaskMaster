5bf52f9622b9b6271b1d959376d0e8c516c15506
Andrew Anter <andrew.anter@gmail.com>
2025-07-02 00:04:16 +0300

    added type hints

--- accounts/views.py
+++ accounts/views.py
@@ -9,18 +9,18 @@ from .forms import CustomLoginForm
 
 
 @login_not_required
-def login_view(request):
-    form = CustomLoginForm()
-
+def login_view(request) -> HttpResponse:
     if request.method == "POST":
         form = CustomLoginForm(request, data=request.POST)
         if form.is_valid():
             user = form.get_user()
             login(request, user)
 
-            response = HttpResponse()
-            response["HX-Location"] = reverse("task_list")
+            response: HttpResponse = HttpResponse()
+            response["HX-Location"] = reverse(viewname="task_list")
             return response
+    else:
+        form = CustomLoginForm()
 
     context = {
         "form": form,
@@ -30,7 +30,7 @@ def login_view(request):
     return render(request=request, template_name="accounts/login.html", context=context)
 
 
-def logout_view(request):
+def logout_view(request) -> HttpResponse:
     if request.method == "POST":
         logout(request=request)
 
