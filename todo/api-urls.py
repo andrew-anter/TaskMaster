efd86935d57f88e13d01613904f09f05ccda8499
Andrew Anter <andrew.anter@gmail.com>
2025-07-26 18:20:31 +0300

    removed excess directory

--- todo/api-urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from .api import DetailUpdateDeleteTaskApiView, ListCreateApiView, ToggleStatusAPI
-
-urlpatterns = [
-    path("", ListCreateApiView.as_view(), name="list-tasks-api"),
-    path("<int:pk>", DetailUpdateDeleteTaskApiView.as_view(), name="task-detail-api"),
-    path("<int:pk>/toggle/", ToggleStatusAPI.as_view(), name="toggle-task-status-api"),
-]
